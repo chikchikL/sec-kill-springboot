bb8cd7a677301091a91e75cbe92c1e1a4d97faa5
liuyang <1071992409@qq.com>
2019-07-24 14:56:43 +0800

    change ignore file

--- src/main/java/com/example/seckill/redis/KeyPrefix.java
+++
@@ -1,9 +0,0 @@
-package com.example.seckill.redis;
-
-public interface KeyPrefix {
-		
-	public int expireSeconds();
-	
-	public String getPrefix();
-	
-}
--- src/main/java/com/example/seckill/redis/OrderKey.java
+++
@@ -1,9 +0,0 @@
-package com.example.seckill.redis;
-
-public class OrderKey extends BasePrefix {
-
-	public OrderKey(int expireSeconds, String prefix) {
-		super(expireSeconds, prefix);
-	}
-
-}

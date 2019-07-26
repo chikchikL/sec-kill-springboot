554f52a3c44dd1d95fbf4020c99031ab3fab2ecb
liuyang <1071992409@qq.com>
2019-07-26 10:31:08 +0800

    商品列表页面缓存

--- src/main/java/com/example/seckill/controller/GoodController.java
+++ src/main/java/com/example/seckill/controller/GoodController.java
@@ -2,15 +2,26 @@ package com.example.seckill.controller;
 
 import com.example.seckill.Vo.GoodsVo;
 import com.example.seckill.domain.MiaoshaUser;
+import com.example.seckill.redis.GoodsKey;
 import com.example.seckill.service.GoodsService;
 import com.example.seckill.service.MiaoshaUserService;
 import com.example.seckill.service.RedisService;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.thymeleaf.context.WebContext;
+import org.thymeleaf.spring5.view.ThymeleafViewResolver;
 
+
+
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
 @Controller
@@ -27,15 +38,37 @@ public class GoodController {
     @Autowired
     GoodsService goodsService;
 
+    @Autowired
+    ThymeleafViewResolver thymeleafViewResolver;
+
     @RequestMapping("/to_list")
-    public String toGoodList(Model model,MiaoshaUser user){
+    @ResponseBody
+    public String toGoodList(HttpServletRequest request, HttpServletResponse response,
+                             Model model, MiaoshaUser user){
         model.addAttribute("user",user);
 
         //查询商品列表
         List<GoodsVo> goodsVos = goodsService.listGoodsVo();
         model.addAttribute("goodslist",goodsVos);
 
-        return "goods_list";
+//        return "goods_list";
+        //取缓存
+        String html = redisService.get(GoodsKey.getGoodsList, "", String.class);
+        if(!StringUtils.isEmpty(html)){
+            return html;
+        }
+
+        //无缓存手动渲染
+        WebContext springWebContext = new WebContext(request, response,
+                request.getServletContext(),
+                request.getLocale(), model.asMap());
+
+        html = thymeleafViewResolver.getTemplateEngine().process("goods_list", springWebContext);
+        if(!StringUtils.isEmpty(html)){
+            redisService.set(GoodsKey.getGoodsList,"",html);
+        }
+
+        return html;
     }
 
     @RequestMapping("/to_detail/{goodsId}")
--- src/main/java/com/example/seckill/domain/OrderInfo.java
+++ src/main/java/com/example/seckill/domain/OrderInfo.java
@@ -16,11 +16,11 @@ public class OrderInfo {
 
     private Integer goodsCount;
 
-    private BigDecimal goodsPrice;
+    private Double goodsPrice;
 
-    private Byte orderChannel;
+    private Integer orderChannel;
 
-    private Byte status;
+    private Integer status;
 
     private Date createDate;
 
@@ -74,27 +74,27 @@ public class OrderInfo {
         this.goodsCount = goodsCount;
     }
 
-    public BigDecimal getGoodsPrice() {
+    public Double getGoodsPrice() {
         return goodsPrice;
     }
 
-    public void setGoodsPrice(BigDecimal goodsPrice) {
+    public void setGoodsPrice(Double goodsPrice) {
         this.goodsPrice = goodsPrice;
     }
 
-    public Byte getOrderChannel() {
+    public Integer getOrderChannel() {
         return orderChannel;
     }
 
-    public void setOrderChannel(Byte orderChannel) {
+    public void setOrderChannel(Integer orderChannel) {
         this.orderChannel = orderChannel;
     }
 
-    public Byte getStatus() {
+    public Integer getStatus() {
         return status;
     }
 
-    public void setStatus(Byte status) {
+    public void setStatus(Integer status) {
         this.status = status;
     }
 
